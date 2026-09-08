7c384b1f6e5bf63aa138c0258d36ca09c5d25b30
kent-stack <kentamadeo0402@gmail.com>
2026-09-08 15:55:31 +0700

    Fix username login validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,11 +16,11 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => ['required', 'string'],
+            'identifier' => ['required', 'string'],
             'password' => ['required'],
         ]);
 
-        $login = $request->input('email');
+        $login = $request->input('identifier');
         $password = $request->input('password');
 
         $credentials = filter_var($login, FILTER_VALIDATE_EMAIL)
@@ -33,8 +33,8 @@ class AuthController extends Controller
         }
 
         return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput('email');
+            'identifier' => 'The provided credentials do not match our records.',
+        ])->onlyInput('identifier');
     }
 
     public function logout(Request $request)
